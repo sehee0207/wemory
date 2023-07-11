c0ce3b080ad7a4c80b3d81ec36bfd6a8ee8b7a91
naahnoey <naahyin20@sookmyung.ac.kr>
2023-07-12 00:40:23 +0900

    password encryption + Member schema modification

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -6,7 +6,6 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
-db.tutorials = require("./tutorial.model.js")(mongoose);
 db.members = require("./member.model.js")(mongoose);
 
 module.exports = db;
--- backend/app/models/member.model.js
+++ backend/app/models/member.model.js
@@ -1,20 +1,49 @@
+const bcrypt = require('bcrypt');
+
 module.exports = mongoose => {
-    var schema = mongoose.Schema(
-      {
-        memberId: String,
-        pw1: String,
-        pw2: String,
-        email: String
+  var schema = mongoose.Schema(
+    {
+      memberId: {
+        type: String,
+        required: true,
+        unique: true
       },
-      { timestamps: true }
-    );
-  
-    schema.method("toJSON", function() {
-      const { __v, _id, ...object } = this.toObject();
-      object.id = _id;
-      return object;
+      pw1: {
+        type: String,
+        required: true
+      },
+      pw2: {
+        type: String,
+        required: true
+      },
+      email: {
+        type: String
+      }
+    },
+    { timestamps: true }
+  );
+
+  schema.pre('save', function(next) {
+    const user = this;
+    const saltFactor = 10;
+    bcrypt.genSalt(saltFactor, (err, salt) => { // create salt
+      if (err) return next(err);
+    
+      bcrypt.hash(user.pw1, salt, (err, hash) => {  // create Hash
+        if (err) return next(err);
+        user.pw1 = hash;  // save Hash
+        user.pw2 = hash;
+        next();
+      });
     });
-  
-    const Member = mongoose.model("member", schema);
-    return Member;
-  };
+  });
+
+  schema.method("toJSON", function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Member = mongoose.model("member", schema);
+  return Member;
+};
